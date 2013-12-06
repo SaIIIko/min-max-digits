93c042ff6bb8b74548cc3d8c68826db14a220ef6
Alexander Voevodskiy <Alexander.altmedia@gmail.com>
2013-12-06 15:50:25 +0300

    Update and rename qwer.js to max-min.js

---
+++ max-min.js
@@ -0,0 +1,30 @@
+/**
+ * Created by alexander on 05.12.13.
+ */
+
+
+/**
+ * @param {number} n Input number
+ * @return {string}
+ */
+var num = function(n) {
+  var max = 0;
+  var min = 9;
+  if (typeof(n) == 'number') {
+    n = Math.abs(n);
+    while (n > 0) {
+      if (n % 10 < min) {
+        min = n % 10;
+      }
+      if (n % 10 > 0) {
+        max = n % 10;
+      }
+      n = Math.floor(n / 10);
+    }
+    return 'Наименьшая цифра: ' + min + '\n'
+    + 'Наибольшая цифра: ' + max;
+  } else {
+    console.log('Должно быть только число');
+  }
+}
+console.log(num(-65747));
